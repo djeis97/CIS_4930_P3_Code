d988775f53894450a5154e3031fec5aa641406ba
Kevin Wagner <KevinWagnerSE@outlook.com>
2016-07-10 18:51:06 -0400

    just a bit of clean up

--- proj2-wagnerk1.cu
+++ proj2-wagnerk1.cu
@@ -1,9 +1,10 @@
 /* ==================================================================
-	Programmer: Yicheng Tu (ytu@cse.usf.edu)
-	GPU algorithm implementation for 3D data
-	To compile: nvcc proj2-wagnerk1-naive-solution.cu -o GPU_V1 in the rc machines
+	Programmers:
+	Kevin Wagner
+	Elijah Malaby
+	John Casey
 	
-	Additional Programming By Kevin Wagner U75723519
+	Omptimizing SDH histograms for input larger then global memory
    ==================================================================
 */
 
@@ -14,7 +15,7 @@
 #include <sys/time.h>
 
 
-#define BOX_SIZE 23000 /* size of the data box on one dimension            */
+#define BOX_SIZE 23000 /* size of the data box on one dimension */
 
 /* descriptors for single atom in the tree */
 typedef struct atomdesc {
@@ -24,44 +25,39 @@ typedef struct atomdesc {
 } atom;
 
 typedef struct hist_entry{
-	//float min;
-	//float max;
-	unsigned long long d_cnt;   /* need a unsigned long long type as the count might be huge */
+	unsigned long long d_cnt;	/* need a unsigned long long type as the count might be huge */
 } bucket;
 
-bucket * histogram;		/* list of all buckets in the histogram   */
-unsigned long long  PDH_acnt;	/* total number of data points            */
-int block_size;			/* Number of threads per block */
-int num_buckets;		/* total number of buckets in the histogram */
-double   PDH_res;		/* value of w                             */
-atom * atom_list;		/* list of all data points                */
+bucket * histogram;		/* list of all buckets in the histogram */
+unsigned long long  PDH_acnt;	/* total number of data points */
+int block_size;		/* Number of threads per block */
+int num_buckets;	/* total number of buckets in the histogram */
+double   PDH_res;	/* value of w */
+atom * atom_list;	/* list of all data points */
 bucket * histogram_GPU;	
 atom * atom_list_GPU;
 	
-
-
 __global__ void GPUKernelFunction (unsigned long long PDH_acnt, double PDH_res, atom * atom_list_GPU, bucket * histogram_GPU) {
 	
+	/* assign register values */
 	int threadID = threadIdx.x + blockIdx.x * blockDim.x;
 	int i, h_pos;
 	double dist;
 	atom temp_atom_1 = atom_list_GPU[threadID];
 	
+	/* loop through all points in atom list calculating distance from current point to all further points */
 	for(i = threadID + 1; i < PDH_acnt; i++)
 	{
 		atom temp_atom_2 = atom_list_GPU[i];
 		dist = sqrt((temp_atom_1.x_pos - temp_atom_2.x_pos) * (temp_atom_1.x_pos - temp_atom_2.x_pos) + 
 					(temp_atom_1.y_pos - temp_atom_2.y_pos) * (temp_atom_1.y_pos - temp_atom_2.y_pos) + 
 					(temp_atom_1.z_pos - temp_atom_2.z_pos) * (temp_atom_1.z_pos - temp_atom_2.z_pos));
-			
 		h_pos = (int)(dist / PDH_res);
 		atomicAdd(&(histogram_GPU[h_pos].d_cnt), 1);
 	}
 }
 
-/* 
-	print the counts in all buckets of the histogram 
-*/
+/* print the counts in all buckets of the histogram  */
 void output_histogram_GPU(){
 	int i; 
 	unsigned long long total_cnt = 0;
@@ -78,19 +74,14 @@ void output_histogram_GPU(){
 }
 
 void GPU_baseline() {
-	
 
-	
-
-	/* allocate GPU histogram */
+	/* copy histogram to device memory */
 	cudaMalloc((void**) &histogram_GPU, sizeof(bucket)*num_buckets);
 	cudaMemcpy(histogram_GPU, histogram, sizeof(bucket)*num_buckets, cudaMemcpyHostToDevice);
 	
 	/* copy atom list to device memory */
 	cudaMalloc((void**) &atom_list_GPU, sizeof(atom) * PDH_acnt);
 	cudaMemcpy(atom_list_GPU, atom_list, sizeof(atom) * PDH_acnt, cudaMemcpyHostToDevice);
-	
-	output_histogram_GPU();
 	
 	/* start time keeping */
 	cudaEvent_t start, stop;
@@ -99,7 +90,7 @@ void GPU_baseline() {
 	cudaEventRecord( start, 0 );
 	
 	/* Run Kernel */
-	GPUKernelFunction <<<(ceil(PDH_acnt + block_size)/block_size), block_size>>> (PDH_acnt, PDH_res, atom_list_GPU, histogram_GPU);
+	GPUKernelFunction <<<((PDH_acnt + block_size)/block_size), block_size>>> (PDH_acnt, PDH_res, atom_list_GPU, histogram_GPU);
 	
 	/* stop time keeping */
 	cudaEventRecord( stop, 0 );
@@ -120,6 +111,7 @@ void GPU_baseline() {
 	cudaEventDestroy( stop );
 }
 
+/* Input Validation Function */
 bool isNumber(char number[], bool floatingpoint)
 {	
     for (int i = 0; number[i] != 0; i++)
@@ -141,6 +133,7 @@ bool isNumber(char number[], bool floatingpoint)
 }
 
 
+/* Most of this input validation can probably be pulled whenever we hardcode our block size and if we hardcode our bucket width */
 int main(int argc, char **argv)
 {
 	/* input validation */
@@ -157,24 +150,21 @@ int main(int argc, char **argv)
 			printf( "Invalid Input Error Invalid Arguments\n Valid input is ./program_name {#of_samples} {bucket_width} {block_size}\n");
 			return 0;
 		}
-
 	}
 	else
 	{
 		printf( "Invalid Input Error Insufficient Arguments\n Valid input is ./program_name {#of_samples} {bucket_width} {block_size}\n");
 		return 0;
 	}
-
 	
-	int i;
-	
+	/* allocate memory */
 	num_buckets = (int)(BOX_SIZE * 1.732 / PDH_res) + 1;
 	histogram = (bucket *)malloc(sizeof(bucket)*num_buckets);
 	atom_list = (atom *)malloc(sizeof(atom)*PDH_acnt);
 	
 	srand(1);
 	/* generate data following a uniform distribution */
-	for(i = 0;  i < PDH_acnt; i++) {
+	for(int i = 0;  i < PDH_acnt; i++) {
 		atom_list[i].x_pos = ((double)(rand()) / RAND_MAX) * BOX_SIZE;
 		atom_list[i].y_pos = ((double)(rand()) / RAND_MAX) * BOX_SIZE;
 		atom_list[i].z_pos = ((double)(rand()) / RAND_MAX) * BOX_SIZE;
