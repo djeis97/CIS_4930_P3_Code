57d822a63625198218c1bc7ef1197bee4e87c5b9
Elijah Malaby <qwe12345678910@gmail.com>
2016-07-12 22:18:50 -0400

    Unable to test changes, but that should do it.

--- proj2-wagnerk1.cu
+++ proj2-wagnerk1.cu
@@ -3,7 +3,7 @@
 	Kevin Wagner
 	Elijah Malaby
 	John Casey
-	
+
 	Omptimizing SDH histograms for input larger then global memory
    ==================================================================
 */
@@ -19,9 +19,9 @@
 
 /* descriptors for single atom in the tree */
 typedef struct atomdesc {
-	double x_pos;
-	double y_pos;
-	double z_pos;
+	float x_pos;
+	float y_pos;
+	float z_pos;
 } atom;
 
 typedef struct hist_entry{
@@ -32,34 +32,61 @@ bucket * histogram;		/* list of all buckets in the histogram */
 unsigned long long  PDH_acnt;	/* total number of data points */
 int block_size;		/* Number of threads per block */
 int num_buckets;	/* total number of buckets in the histogram */
-double   PDH_res;	/* value of w */
+float   PDH_res;	/* value of w */
 atom * atom_list;	/* list of all data points */
-bucket * histogram_GPU;	
+bucket * histogram_GPU;
+bucket * temp_histogram_GPU;
 atom * atom_list_GPU;
-	
-__global__ void GPUKernelFunction (unsigned long long PDH_acnt, double PDH_res, atom * atom_list_GPU, bucket * histogram_GPU) {
-	
+
+__global__ void kernelSumHistogram( unsigned long long int *InputHists, unsigned long long int *hist, int num_atoms, int num_buckets, int block_size) {
+  ulli tid = threadIdx.x + blockIdx.x * blockDim.x;
+  int h_pos = tid;
+  unsigned long long int NumberOfSumLoop = 0;
+  NumberOfSumLoop = (num_atoms)/block_size + ((num_atoms%block_size) ? 1:0);
+
+  while(h_pos < num_buckets) {
+    unsigned long long int tmpAns = 0;
+    for(int i=0;i<NumberOfSumLoop;i++){
+      tmpAns = tmpAns + *(InputHists+(i*num_buckets)+h_pos);
+    }
+    hist[h_pos] = tmpAns;
+    h_pos += blockDim.x * gridDim.x;
+  }
+  __syncthreads();
+}
+
+__global__ void GPUKernelFunction (unsigned long long PDH_acnt, float PDH_res, atom * atom_list_GPU, bucket * histogram_GPU, int num_buckets) {
+
+  extern __shared__ ulli SHist[];
 	/* assign register values */
 	int threadID = threadIdx.x + blockIdx.x * blockDim.x;
 	int i, h_pos;
-	double dist;
+	float dist;
 	atom temp_atom_1 = atom_list_GPU[threadID];
-	
+
+  for(h_pos=threadIdx.x; h_pos < num_buckets; h_pos+=blockDim.x)
+    SHist[h_pos] = 0;
+
+  __syncthreads();
+
 	/* loop through all points in atom list calculating distance from current point to all further points */
 	for(i = threadID + 1; i < PDH_acnt; i++)
 	{
 		atom temp_atom_2 = atom_list_GPU[i];
-		dist = sqrt((temp_atom_1.x_pos - temp_atom_2.x_pos) * (temp_atom_1.x_pos - temp_atom_2.x_pos) + 
-					(temp_atom_1.y_pos - temp_atom_2.y_pos) * (temp_atom_1.y_pos - temp_atom_2.y_pos) + 
+		dist = sqrt((temp_atom_1.x_pos - temp_atom_2.x_pos) * (temp_atom_1.x_pos - temp_atom_2.x_pos) +
+					(temp_atom_1.y_pos - temp_atom_2.y_pos) * (temp_atom_1.y_pos - temp_atom_2.y_pos) +
 					(temp_atom_1.z_pos - temp_atom_2.z_pos) * (temp_atom_1.z_pos - temp_atom_2.z_pos));
 		h_pos = (int)(dist / PDH_res);
-		atomicAdd(&(histogram_GPU[h_pos].d_cnt), 1);
+		atomicAdd(&(SHist[h_pos]), 1);
 	}
+  __syncthreads();
+  for(h_pos = threadIdx.x; h_pos < num_buckets; h_pos += blockdim.x)
+    *(histogram_GPU+(num_buckets*blockIdx.x)+h_pos).d_cnt += SHist[h_pos];
 }
 
 /* print the counts in all buckets of the histogram  */
 void output_histogram_GPU(){
-	int i; 
+	int i;
 	unsigned long long total_cnt = 0;
 	for(i=0; i< num_buckets; i++) {
 		if(i%5 == 0) /* we print 5 buckets in a row */
@@ -67,7 +94,7 @@ void output_histogram_GPU(){
 		printf("%15lld ", histogram[i].d_cnt);
 		total_cnt += histogram[i].d_cnt;
 	  	/* we also want to make sure the total distance count is correct */
-		if(i == num_buckets - 1)	
+		if(i == num_buckets - 1)
 			printf("\n T:%lld \n", total_cnt);
 		else printf("| ");
 	}
@@ -77,43 +104,49 @@ void GPU_baseline() {
 
 	/* copy histogram to device memory */
 	cudaMalloc((void**) &histogram_GPU, sizeof(bucket)*num_buckets);
-	cudaMemcpy(histogram_GPU, histogram, sizeof(bucket)*num_buckets, cudaMemcpyHostToDevice);
-	
+	cudaMemset(histogram_GPU, 0, sizeof(bucket)*num_buckets, cudaMemcpyHostToDevice);
+	cudaMalloc((void**) &temp_histogram_GPU, sizeof(bucket)*num_buckets*num_blocks);
+	cudaMemset(temp_histogram_GPU, 0, sizeof(bucket)*num_buckets*num_blocks, cudaMemcpyHostToDevice);
+
+
 	/* copy atom list to device memory */
 	cudaMalloc((void**) &atom_list_GPU, sizeof(atom) * PDH_acnt);
 	cudaMemcpy(atom_list_GPU, atom_list, sizeof(atom) * PDH_acnt, cudaMemcpyHostToDevice);
-	
+
 	/* start time keeping */
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 	cudaEventRecord( start, 0 );
-	
+
 	/* Run Kernel */
-	GPUKernelFunction <<<((PDH_acnt + block_size)/block_size), block_size>>> (PDH_acnt, PDH_res, atom_list_GPU, histogram_GPU);
-	
+	GPUKernelFunction <<<((PDH_acnt + block_size)/block_size), block_size, sizeof(bucket)*num_buckets>>> (PDH_acnt, PDH_res, atom_list_GPU, temp_histogram_GPU, num_buckets);
+  cudaDeviceSynctonize();
+  kernelSumHistogram<<<3, 512>>>(temp_histogram_GPU, histogram_GPU, num_atoms, num_buckets, block_size);
+
 	/* stop time keeping */
 	cudaEventRecord( stop, 0 );
 	cudaEventSynchronize( stop );
 	float elapsedTime;
 	cudaEventElapsedTime( &elapsedTime, start, stop );
-	
+
 	/* transfer histogram to host memory */
 	cudaMemcpy(histogram, histogram_GPU, sizeof(bucket)*num_buckets, cudaMemcpyDeviceToHost);
-	
+
 	/* print out the histogram */
 	output_histogram_GPU();
 	elapsedTime = elapsedTime/1000;
 	printf( "******** Total Running Time of Kernel = %0.5f sec *******\n", elapsedTime );
-	
+
 	/* free cuda timekeeping */
 	cudaEventDestroy( start );
 	cudaEventDestroy( stop );
+  cudaFree(temp_histogram_GPU);
 }
 
 /* Input Validation Function */
 bool isNumber(char number[], bool floatingpoint)
-{	
+{
     for (int i = 0; number[i] != 0; i++)
     {
         //if (number[i] > '9' || number[i] < '0')
@@ -156,27 +189,27 @@ int main(int argc, char **argv)
 		printf( "Invalid Input Error Insufficient Arguments\n Valid input is ./program_name {#of_samples} {bucket_width} {block_size}\n");
 		return 0;
 	}
-	
+
 	/* allocate memory */
 	num_buckets = (int)(BOX_SIZE * 1.732 / PDH_res) + 1;
 	histogram = (bucket *)malloc(sizeof(bucket)*num_buckets);
 	atom_list = (atom *)malloc(sizeof(atom)*PDH_acnt);
-	
+
 	srand(1);
 	/* generate data following a uniform distribution */
 	for(int i = 0;  i < PDH_acnt; i++) {
-		atom_list[i].x_pos = ((double)(rand()) / RAND_MAX) * BOX_SIZE;
-		atom_list[i].y_pos = ((double)(rand()) / RAND_MAX) * BOX_SIZE;
-		atom_list[i].z_pos = ((double)(rand()) / RAND_MAX) * BOX_SIZE;
+		atom_list[i].x_pos = ((float)(rand()) / RAND_MAX) * BOX_SIZE;
+		atom_list[i].y_pos = ((float)(rand()) / RAND_MAX) * BOX_SIZE;
+		atom_list[i].z_pos = ((float)(rand()) / RAND_MAX) * BOX_SIZE;
 	}
-	
+
 	/* call GPU histrogram compute */
 	GPU_baseline();
 
 	/* free memory */
 	free(histogram);
 	free(atom_list);
-	
+
 	return 0;
 }
 
